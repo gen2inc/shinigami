4a8d813f4d34ab3738f56811336992fcb17c73b9
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2023-07-09 11:40:41 -0700

    add decryption tool and improve index

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import express from 'express';
-import jwt from "jsonwebtoken";
+import fs from "fs"
 const app = express()
 
+/*
+  do the decryption process in the cloud or do it locally,
+  if via the cloud, please provide your decryption keys
+*/
+const DECRYPTION_PROCESS = false;
+
+app.use(express.json());
+
 app.post('/submit-vote', (req, res) => {
+    if (DECRYPTION_PROCESS) {
+      
+    } else {
+      // fs.writeFileSync("logs.txt", )
+    }
     res.send('SUCCESS')
 })
 
+app.get('/dat', (req, res) => {
+  
+})
+
 app.listen(3000, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Shinigami loaded and listening on port 3000`)
 })
