3b79362ed75549cc6ef0cd5986ae025c98d37c06
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2023-07-11 17:41:11 -0700

    fix smol lil bug

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 app.post('/submit-vote', (req, res) => {
   if (SERVER_DECRYPTION) {
     let decrypt = decryptText(Buffer.from(req.body.enc, 'base64'));
-    fs.appendFileSync("./logs.txt", decrypt.toString());
+    fs.appendFileSync("./logs.txt", `${decrypt.toString()}\n`);
   } else {
-    fs.appendFileSync("./logs.txt", req.body);  
+    fs.appendFileSync("./logs.txt", `${req.body}\n`);  
   }
   res.send('SUCCESS')
 })
